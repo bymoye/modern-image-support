db815ff7d09435e97febfd295628cf966935ec63
nazo <s3moye@gmail.com>
2024-12-11 23:14:46 +0800

    优化

--- webp_support/webp_support_c.c
+++ webp_support/webp_support_c.c
@@ -3,21 +3,23 @@
 #include <ctype.h>
 #include <stdio.h>
 #include <stdbool.h>
-struct browser_version
-{
+
+struct browser_version {
     const char *name;
     int min_version;
+    size_t name_len;
 };
 
 static const struct browser_version browser_versions[] = {
-    {"Firefox", 65},
-    {"Chrome", 32},
-    {"Edge", 18},
-    {"AppleWebKit", 605},
-    {"OPR", 19},
-    {"UCBrowser", 12},
-    {"SamsungBrowser", 4},
-    {"QQBrowser", 10}};
+    {"Firefox", 65, 7},
+    {"Chrome", 32, 6},
+    {"Edge", 18, 4},
+    {"AppleWebKit", 605, 11},
+    {"OPR", 19, 3},
+    {"UCBrowser", 12, 9},
+    {"SamsungBrowser", 4, 14},
+    {"QQBrowser", 10, 9}
+};
 
 #define BROWSER_VERSION_COUNT (sizeof(browser_versions) / sizeof(browser_versions[0]))
 
@@ -28,23 +30,23 @@ bool is_webp_supported(const char *user_agent)
         return false;
     }
 
-    const char *found;
-    const char *version;
-    int version_number;
-    for (size_t i = 0; i < BROWSER_VERSION_COUNT; i++)
+    const size_t count = BROWSER_VERSION_COUNT;
+    
+    for (size_t i = 0; i < count; i++)
     {
-        found = strstr(user_agent, browser_versions[i].name);
+        const struct browser_version *current_browser = &browser_versions[i];
+        const char *found = strstr(user_agent, current_browser->name);
         if (found != NULL)
         {
-            version = found + strlen(browser_versions[i].name);
+            const char *version = found + current_browser->name_len;
             while (!isdigit(*version) && *version != '\0')
             {
                 version++;
             }
             if (*version != '\0')
             {
-                version_number = atoi(version);
-                if (version_number >= browser_versions[i].min_version)
+                int version_number = atoi(version);
+                if (version_number >= current_browser->min_version)
                 {
                     return true;
                 }
