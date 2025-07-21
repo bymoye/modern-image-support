15ddc0b744d8a5703b6ee969db7398d3d4a40295
bymoye <s3moye@gmail.com>
2025-07-21 12:33:28 +0800

    fix

--- setup.py
+++ setup.py
@@ -17,7 +17,6 @@ ext_modules = [
 ]
 
 setup(
-    name="modern-image-support",
     ext_modules=cythonize(
         ext_modules,
         language_level=3,
@@ -36,30 +35,5 @@ setup(
         ]
     },
     include_package_data=True,
-    author="bymoye",
-    author_email="s3moye@gmail.com",
-    version="0.4.0",
-    url="https://github.com/bymoye/modern-image-support",
-    description="Fast WebP and AVIF image format support detection from User-Agent strings.",
-    long_description=readme(),
-    long_description_content_type="text/markdown",
-    license="MIT",
-    classifiers=[
-        "Programming Language :: Python :: 3",
-        "Programming Language :: Python :: 3.9",
-        "Programming Language :: Python :: 3.10",
-        "Programming Language :: Python :: 3.11",
-        "Programming Language :: Python :: 3.12",
-        "Programming Language :: Python :: 3.13",
-        "Programming Language :: Python :: 3.14",
-        "Programming Language :: Cython",
-        "Programming Language :: C",
-        "Topic :: Internet :: WWW/HTTP",
-        "Topic :: Multimedia :: Graphics",
-        "Topic :: Software Development :: Libraries :: Python Modules",
-        "Operating System :: OS Independent",
-    ],
-    python_requires=">=3.9",
     packages=["modern_image_support"],
-    keywords=["webp", "avif", "image", "user-agent", "browser-detection"],
 )
